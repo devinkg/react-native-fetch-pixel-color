342402ea3dcc339dd7649bef2f2feb8a934b8c4d
Devin Godage <devinkanilka@gmail.com>
2023-04-24 01:40:17 +0530

    typescript issues resolved setting any as all the params

--- src/index.tsx
+++ src/index.tsx
@@ -17,15 +17,15 @@ const FetchPixelColor = NativeModules.FetchPixelColor
       }
     );
 
-const rgb2hex = (rgb) => {
+const rgb2hex = (rgb: string | any[]) => {
   return (rgb && rgb.length === 3) ? '#' +
     ('0' + parseInt(rgb[0], 10).toString(16)).slice(-2) +
     ('0' + parseInt(rgb[1], 10).toString(16)).slice(-2) +
     ('0' + parseInt(rgb[2], 10).toString(16)).slice(-2) : '';
 };
 
-export const setImageAndroid = (path) => new Promise((resolve, reject) => {
-  FetchPixelColor.setImage(path, (err, isSet) => {
+export const setImageAndroid = (path: any) => new Promise((resolve, reject) => {
+  FetchPixelColor.setImage(path, (err: any, isSet: any) => {
     if (err) {
       return reject(err);
     }
@@ -35,8 +35,8 @@ export const setImageAndroid = (path) => new Promise((resolve, reject) => {
   });
 });
 
-export const pickColorOfPixelAndroid = (x, y) => new Promise((resolve, reject) => {
-  FetchPixelColor.getRGB(x, y, (err, color) => {
+export const pickColorOfPixelAndroid = (x: any, y: any) => new Promise((resolve, reject) => {
+  FetchPixelColor.getRGB(x, y, (err: any, color: any) => {
     if (err) {
       return reject(err);
     }
