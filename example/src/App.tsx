35b503176888e5332b51b6d9fe282be58bd10021
Devin Godage <devinkanilka@gmail.com>
2023-04-24 01:59:38 +0530

    example project changed

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,13 +1,13 @@
 import * as React from 'react';
 
 import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'react-native-fetch-pixel-color';
+// import { multiply } from 'react-native-fetch-pixel-color';
 
 export default function App() {
   const [result, setResult] = React.useState<number | undefined>();
 
   React.useEffect(() => {
-    multiply(3, 7).then(setResult);
+    
   }, []);
 
   return (
